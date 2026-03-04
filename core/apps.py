87c00532951a267efbe27e93b5b578b75ba91157
Abisin-Raj <abisinr201@gmail.com>
2026-03-04 17:20:52 +0530

    Implement apps - Part 2

--- core/apps.py
+++ core/apps.py
@@ -7,3 +7,12 @@ class CoreConfig(AppConfig):
 
     def ready(self):
         # Apply sqlite pragmas in case sqlite is used
+        try:
+            from django.db import connection
+            if connection.vendor == "sqlite":
+                with connection.cursor() as cur:
+                    cur.execute("PRAGMA journal_mode=WAL;")
+                    cur.execute("PRAGMA synchronous=NORMAL;")
+                    cur.execute("PRAGMA foreign_keys=ON;")
+        except Exception:
+            pass

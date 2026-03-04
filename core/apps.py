483a9acad471f6d47d3da66cac1d7e522836aba6
Abisin-Raj <abisinr201@gmail.com>
2026-03-04 17:19:18 +0530

    Update apps.py: Add functionality part 2

--- core/apps.py
+++ core/apps.py
@@ -9,3 +9,14 @@ class CoreConfig(AppConfig):
         # Apply sqlite pragmas in case sqlite is used
         try:
             from django.db import connection
+            if connection.vendor == "sqlite":
+                with connection.cursor() as cur:
+                    cur.execute("PRAGMA journal_mode=WAL;")
+                    cur.execute("PRAGMA synchronous=NORMAL;")
+                    cur.execute("PRAGMA foreign_keys=ON;")
+        except Exception:
+            pass
+
+        # Avoid running during migrations or if not root (will log warning)
+        try:
+            from . import firewall
